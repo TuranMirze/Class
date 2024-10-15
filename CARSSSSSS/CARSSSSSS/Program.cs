7d379a9de3b2661a0a8b29ec6ce7bc3cb089d8a6
TuranMirze <turanam-bp215@code.edu.az>
2024-10-15 12:47:32 +0400

    Class

---
+++ CARSSSSSS/CARSSSSSS/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CARSSSSSS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Hyundai", "Elantra", 15000, 2);
+            car1.ShowFullInfo();
+            Car car2 = new Car("MercedeS", "4goz", 23000, 3);
+            car2.ShowFullInfo();
+            Car car3 = new Car("Lambo", "Urus", 300, 2);
+            car3.ShowFullInfo();
+
+            MotorCycle moto1 = new MotorCycle("Yakohoma", "Harley", 6000, 2);
+            moto1.ShowFullInfo();
+            MotorCycle moto2 = new MotorCycle("Yakohoma", "Sparetime", 14000, 2);
+            moto2.ShowFullInfo();
+            MotorCycle moto3 = new MotorCycle("Yakohoma", "Geolander", 42000, 2);
+            moto3.ShowFullInfo();
+        }
+        class Vehicle
+        {
+            public string Brand;
+            public string Model;
+            public double Mile;
+            public Vehicle(string brand, string model, double mile)
+            {
+                if ((brand == " " || brand == "") || (model == "" || model == " "))
+                {
+                    Console.WriteLine("Melumatlari daxil edin");
+
+                }
+                Brand = brand;
+                Model = model;
+                Mile = mile;
+            }
+            public void ShowFullInfo()
+            {
+                Console.WriteLine($"Brand:{Brand},Model:{Model},Mile:{Mile}");
+            }
+        }
+
+        class Car : Vehicle
+        {
+            public int DoorsNum;
+            public Car(string brand, string model, double mile, int doorsNum) : base(brand, model, mile)
+            {
+                DoorsNum = doorsNum;
+            }
+
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine($"Qapi sayi: {DoorsNum}");
+            }
+        }
+
+        class MotorCycle : Vehicle
+        {
+            public int WheelNum;
+
+            public MotorCycle(string brand, string model, double mile, int wheelNum) : base(brand, model, mile)
+            {
+                WheelNum = wheelNum;
+            }
+
+            public void ShowFullInfo()
+            {
+                base.ShowFullInfo();
+                Console.WriteLine($"Teker sayi: {WheelNum}");
+            }
+        }
+
+    }
+}
---
+++ TexnoImperia/TexnoImperia/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace CompStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook Acer = new Notebook("Asus", "Gaming", 7, 4500, 32, 512);
+            Acer.ShowFullData();
+            Notebook Hp = new Notebook("Hp", "Office", 16, 1200, 8, 256);
+            Hp.ShowFullData();
+            Notebook Lenovo = new Notebook("Lenovo", "Office" , 30, 1000, 16, 512);
+            Lenovo.ShowFullData();
+        }
+        class Product
+        {
+            public string Name;
+            public string Description;
+            public int Count;
+            public bool IsStock;
+            public double Price;
+
+            public Product(string name, string description, int count, double price)
+            {
+                Name = name;
+                Description = description;
+                Count = count;
+                Price = price;
+            }
+        
+            public void ShowFullData()
+            {
+                Console.WriteLine($"Name:{Name},Description:{Description},Price:{Price},Count:{Count},Price:{Price}");
+            }
+
+        }
+        class Notebook : Product
+        {
+            public byte Ram;
+            public int Storage;
+            public Notebook(string name, string description, int count, double price, byte ram, int storage) : base(name, description, count, price)
+            {
+               
+                if (count == 0)
+                {
+                    Console.WriteLine("Mehsul stokda yoxdu");
+                }
+                Ram = ram;
+                Storage = storage;
+
+            }
+            public void ShowFullData()
+            {
+                base.ShowFullData();
+                Console.WriteLine($"Ram:{Ram},Storage:{Storage}");
+            }
+
+        }
+    }
+}
